41ded9b47cf2d7df0daf8fb6d7d01e1f93f0d04a
yanitejeda <tejedasp@gmail.com>
2021-11-18 18:10:20 +0100

    1 / 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="query" placeholder="Cerca.."> <button @click="buttonSerch">Search</button>
+    <div class="card-movies">
+      <ul>
+        <li v-for='movie in movies' :key='movie.id' >
+          <h2> {{movie.title}} </h2>
+          <h3> {{movie.original_title}} </h3>
+          <h4>{{movie.original_language}} </h4>
+          <img :src="flag[movie.original_language]" alt="">
+          <h5>{{movie.vote_average}}</h5>
+        </li>
+      </ul>
+
+      <ul>
+        <li v-for='serie in series' :key='serie.id' >
+          <h2> {{serie.name}} </h2>
+          <h3> {{serie.original_name}} </h3>
+          <h4>{{serie.original_language}} </h4>
+          <img :src="flag[serie.original_language]" alt="">
+          <h5>{{serie.vote_average}}</h5>
+        </li>
+      </ul>
+      
+      
+    </div>
+   
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data(){
+    return{
+      apiKey: "ca7354ccd689f61ad128617804bd67af",
+      apiUrl:"https://api.themoviedb.org/3",
+      movies:[],
+      series:[],
+
+      query:"",
+      flag:{
+        it:require('./assets/it.png'),
+        en:require('./assets/en.png'),
+}
+    };
+  },
+
+  methods:{
+    buttonSerch(){
+
+      //chiamata axios per avere la lista di oggetti
+      axios.get(this.apiUrl +"/search/movie",{
+        params:{
+          api_key : this.apiKey,
+
+          //ricerca
+          query: this.query,
+          //scegliere la lingua da stampare
+          language:"it",
+        },
+      })
+
+      .then((resp)=>{
+        this.movies = resp.data.results;
+
+      })
+
+       axios.get(this.apiUrl +"/search/tv",{
+        params:{
+          api_key : this.apiKey,
+
+          //ricerca
+          query: this.query,
+          //scegliere la lingua da stampare
+          language:"it",
+        },
+      })
+
+      .then((resp)=>{
+        this.series = resp.data.results;
+
+      })
+
+
+
+
+    }
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
