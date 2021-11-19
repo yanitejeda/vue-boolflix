5f7a687fa56107e2914d94025089de45669a8e54
yanitejeda <tejedasp@gmail.com>
2021-11-19 19:18:39 +0100

    errori da correggere

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,100 @@
 <template>
   <div id="app">
-   <!--  <span>
+    <!--  <span>
       <i class="far fa-star"></i>
        stella vuota 
       <i class="fa solid fa-star"></i>
       stella piena 
     </span>
  -->
-    <input type="text" v-model="query" placeholder="Cerca.." />
-    <button @click="buttonSerch">Search</button>
-    <div class="card-movies">
-      <ul>
-        <li v-for="movie in movies" :key="movie.id">
-          <h2>{{ movie.title }}</h2>
-          <h3>{{ movie.original_title }}</h3>
+    <!-- nav -->
+    <div class="box-container">
+      <div>
+        <h1>BoolFlix</h1>
+      </div>
+      <div>
+        <input type="text" v-model="query" placeholder="Cerca.." />
+        <button @click="buttonSerch">Search</button>
+      </div>
+    </div>
 
-          <!-- (let i = 0; i < array.length; i++) -->
-          <!--  <i class="far fa-star"></i> -->
-          <!-- stella vuota -->
-          <div>
-            <i v-for="i in Math.floor(movie.vote_average / 2)" :key="i"   class="fa solid fa-star"></i>
+    <div class="card-movies box-container-card">
+      <div>
+        <h1>movies</h1>
+        <ul>
+          <li class="card-box" v-for="movie in movies" :key="movie.id">
+            <div class="box-img">
+              <h3 class="tittle">{{ movie.title }}</h3>
+              <!--   <div>{{imgBox(movie.poster_path)}}</div> -->
+              <img :src="imgBox(movie.poster_path)" alt="" />
 
-            <i v-for="i in (5 - Math.floor(movie.vote_average / 2))" :key="i" class="far fa-star"></i>
-         </div>
+              <div class="hover-p">
+                <h3>{{ movie.original_title }}</h3>
+                <h5>{{ movie.vote_average }}</h5>
 
+                <div>
+                  <i
+                    v-for="i in Math.floor(movie.vote_average / 2)"
+                    :key="i"
+                    class="fa solid fa-star"
+                  ></i>
+                  <i
+                    v-for="i in 5 - Math.floor(movie.vote_average / 2)"
+                    :key="i"
+                    class="far fa-star"
+                  ></i>
+                </div>
+                <div>
+                  <h4>{{ movie.original_language }}</h4>
+                  <img
+                    class="flag-w"
+                    :src="flag[movie.original_language]"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
 
-           <!--   <div>{{imgBox(movie.poster_path)}}</div> -->
-          <img :src="imgBox(movie.poster_path)" alt="" />
-          <h4>{{ movie.original_language }}</h4>
-          <img :src="flag[movie.original_language]" alt="" />
+      <div>
+        <h1>series</h1>
+        <ul>
+          <li class="card-box" v-for="serie in series" :key="serie.id">
+            <div class="box-img">
+              <h3 class="tittle">{{ serie.name }}</h3>
+              <img :src="imgBox(serie.poster_path)" alt="" />
+              <div class="hover-p">
+                <h3>{{ serie.original_name }}</h3>
+                <h5>{{ serie.vote_average }}</h5>
 
-          <h5>{{ movie.vote_average }}</h5>
-          <h5>{{ Math.floor(movie.vote_average / 2) }}</h5>
-        </li>
-      </ul>
+                <div>
+                  <i
+                    v-for="i in Math.floor(serie.vote_average / 2)"
+                    :key="i"
+                    class="fa solid fa-star"
+                  ></i>
+                  <i
+                    v-for="i in 5 - Math.floor(serie.vote_average / 2)"
+                    :key="i"
+                    class="far fa-star"
+                  ></i>
+                </div>
 
-      <ul>
-        <li v-for="serie in series" :key="serie.id">
-          <h2>{{ serie.name }}</h2>
-          <h3>{{ serie.original_name }}</h3>
-          <!--   <div>{{imgBox(serie.poster_path)}}</div> -->
-
-          <!-- <i class="far fa-star"></i> -->
-          <!-- stella vuota -->
-          <!-- <i class="fa solid fa-star"></i> -->
-          <img :src="imgBox(serie.poster_path)" alt="" />
-
-          <h4>{{ serie.original_language }}</h4>
-          <img :src="flag[serie.original_language]" alt="" />
-
-
-          <h5>{{ serie.vote_average }}</h5>
-          <h5>{{ Math.floor(serie.vote_average / 2) }}</h5>
-
-
-
-          <div>
-            <i v-for="i in Math.floor(serie.vote_average / 2)" :key="i"   class="fa solid fa-star"></i>
-
-            <i v-for="i in (5 - Math.floor(serie.vote_average / 2))" :key="i" class="far fa-star"></i>
-          </div>
-
-         <!--  <h5>{{ Math.floor(serie.vote_average / 2) }}</h5> -->
-        </li>
-      </ul>
+                <div>
+                  <h4>{{ serie.original_language }}</h4>
+                  <img
+                    class="flag-w"
+                    :src="flag[serie.original_language]"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -88,10 +119,7 @@ export default {
         it: require("./assets/it.png"),
         en: require("./assets/en.png"),
       },
-
-      
     };
-    
   },
 
   methods: {
@@ -138,21 +166,16 @@ export default {
         });
     },
 
-    starC(){
-      return Math.floor(this.movie.vote_average)
-    }
-
-    
-
-
+    starC() {
+      return Math.floor(this.movie.vote_average);
+    },
   },
-
-  
 };
 </script>
 
 <style lang="scss">
 @import "@/styles/app";
+@import "@/styles/appStyle";
 </style>
 
 
